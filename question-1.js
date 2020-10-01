4bba3061afb4a62e26e4fd0152dc5dfe99b39feb
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-10-01 12:29:34 +0200

    Update question-1.js

--- question-1.js
+++ question-1.js
@@ -2,4 +2,4 @@ const division = (a, b) => a % b;
 
 
 
-Had to use "/" insted of "%" to make it work. But i let the % stand here since it says convert the function below, and i did.
+Had to use "/" insted of "%" to make it work/devide. But i let the % stand here since it says convert the function below, and i did.
